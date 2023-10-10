7a4f3eeb8fe5463ca39c8923a3ef444bb0ce04cb
rnphum006 <119986865+rnphum006@users.noreply.github.com>
2023-10-10 15:40:36 +0700

    .commit

--- Home.py
+++ Home.py
@@ -2,8 +2,8 @@ import json
 import time
 import requests
 import streamlit as st
-#from streamlit_lottie import st_lottie
-#from streamlit_lottie import st_lottie_spinner
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
 
 def load_lottieurl(url: str):
     r = requests.get(url)
@@ -18,9 +18,9 @@ st.subheader("Ronnapoom Ponpai DS NPRU")
 col1, col2 = st.columns(2)
 with col1:
     st.image('./pic/phum.jpg')
-    #lot3="https://lottie.host/defef63f-5b59-455d-a6e0-ff03f2791104/t43d8wbLKq.json"
-    #lottie3 = load_lottieurl(lot3)
-    #st_lottie(lottie3)
+    lot3="https://lottie.host/c72fdf8f-9abf-4eff-b107-d930d6f67a15/vc7Me09Q6e.json"
+    lottie3 = load_lottieurl(lot3)
+    st_lottie(lottie3)
 with col2:
     st.image('./pic/DS1.jpg')
 st.balloons()
